e5097cba463c44f98d4af061cefdb04a3f91fb21
mahadihasan2 <mahadihasan125643@gmail.com>
2022-04-23 23:25:20 +0600

    service-data-load-done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import About from './pages/About/About';
+import Home from './pages/Home/Home/Home';
+import Footer from './pages/Sheard/Footer/Footer';
+import Header from './pages/Sheard/Header/Header';
 
 function App() {
   return (
-    <div className="App">
-
+    <div>
+      <Header></Header>
+      <Routes>
+        <Route path='/' element={<Home></Home>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
+      </Routes>
+      <Footer></Footer>
     </div>
   );
 }
