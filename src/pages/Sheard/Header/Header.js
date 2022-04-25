0524a824a9a22ced7f80055717b752c414f914e3
mahadihasan2 <mahadihasan125643@gmail.com>
2022-04-26 05:12:18 +0600

    navBar done

--- src/pages/Sheard/Header/Header.js
+++ src/pages/Sheard/Header/Header.js
@@ -1,10 +1,24 @@
 import React from 'react';
-
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import banner1 from '../../../images/logo/logo1.png';
 const Header = () => {
     return (
-        <div>
-            <h2>This is the Header </h2>
-        </div>
+        <>
+            <Navbar bg="white">
+                <Container>
+                    <Navbar.Brand href="#home">
+                        <img style={{ width: '50px' }} src={banner1} alt="" />
+                    </Navbar.Brand>
+
+                    <Nav className="me-auto">
+                        <Nav.Link href="#home">Home</Nav.Link>
+                        <Nav.Link href="#features">Features</Nav.Link>
+                        <Nav.Link href="#pricing">Pricing</Nav.Link>
+                    </Nav>
+                </Container>
+            </Navbar>
+
+        </>
     );
 };
 
